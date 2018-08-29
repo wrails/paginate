93249308b6e1c8471695c28c899801cd0ffe0286
tesdeve <tesdeve@gmail.com>
2018-08-29 14:58:56 +0100

    Adds 1 as a minimun in @page in case no data is entered

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -3,7 +3,7 @@ class NumbersController < ApplicationController
 		if(params[:numbers_per_page] && params[:page])
 	   
 	   		@numbers_per_page = [1, params[:numbers_per_page].to_i].max
-	  		@page = helpers.page_limit(@numbers_per_page, params[:page].to_i)
+	  		@page = helpers.page_limit(@numbers_per_page, [1, params[:page].to_i].max)
 
 	 		@numbers = helpers.create_array(@numbers_per_page, @page)
 	 		@last_page = helpers.last_page(@numbers_per_page)
